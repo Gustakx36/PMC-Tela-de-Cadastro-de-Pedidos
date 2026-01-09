50e6290e28345ff4ff9a939a138a7084b8f06b82
Gustakx36 <gustavobruno12333@gmail.com>
2026-01-09 14:13:48 -0300

    fix: Ajustando Acesso Smartphone

--- public/view/login/index.php
+++ public/view/login/index.php
@@ -9,8 +9,9 @@ require __DIR__ . '/../../../app/validacao/validacao.php';
     <title>Login</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body class="bg-light">
 
